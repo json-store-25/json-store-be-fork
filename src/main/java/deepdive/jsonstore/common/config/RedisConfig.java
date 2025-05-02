80adb6d72b5b99aed8dd09e5342da4240bbe46e7
cupokki <nd1346@gmail.com>
2025-05-02 17:25:55 +0900

    feat : LocalTimeStamp 직렬화 설정, 직렬화 모두허용

--- src/main/java/deepdive/jsonstore/common/config/RedisConfig.java
+++ src/main/java/deepdive/jsonstore/common/config/RedisConfig.java
@@ -1,20 +1,49 @@
 package deepdive.jsonstore.common.config;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import deepdive.jsonstore.domain.product.dto.ProductCache;
+import deepdive.jsonstore.domain.product.dto.ProductResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate(
+            RedisConnectionFactory connectionFactory,
+            ObjectMapper objectMapper
+    ) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        //ObjectMapper 커스텀
+        ObjectMapper copy = objectMapper.copy();
+        copy.registerModule(new JavaTimeModule());
+        copy.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); //LocalDateTime
+
+        // 타입 정보 포함시켜야 역직렬화 가능
+        copy.activateDefaultTyping(
+                copy.getPolymorphicTypeValidator(),
+                ObjectMapper.DefaultTyping.EVERYTHING,
+                JsonTypeInfo.As.PROPERTY
+        );
+
+        // 커스터마이징된 ObjectMapper로 RedisSerializer 생성
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(copy);
+        template.setValueSerializer(serializer);
+        template.setHashValueSerializer(serializer);
+
         return template;
     }
 }
